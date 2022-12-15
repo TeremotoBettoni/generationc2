5d2ab1c5eac92f103ff6b57374bb54d16b4ad644
TeremotoBettoni <mtbettoni@gmail.com>
2022-12-15 16:34:51 -0300

    estructura base para absorver api axios

---
+++ react/myapp/src/services/AutoServices.js
@@ -0,0 +1,20 @@
+//axios
+import axios from 'axios';
+
+//establecer la ruta por default
+
+const rutaBase = "http://localhost:9080/api2";
+
+// funcion de el llamado
+
+const getAllAutos =async()=>{ // peticion asincrona que se ejecutara independientemente
+    const respuesta = await axios.get(rutaBase+"/autos/getall"); // await significa que esta espernaod la respuesta
+    console.log(respuesta.data);
+    return respuesta.data;
+}
+
+const getAutos =async(id)=>{ // peticion asincrona que se ejecutara independientemente
+    const respuesta = await axios.get(rutaBase+"/obtener/auto/"+id); // await significa que esta espernaod la respuesta
+    console.log(respuesta.data);
+    return respuesta.data;
+}
