5498fd71dcd7fb1f72cf8b56a60c870fbfbd3eea
TeremotoBettoni <mtbettoni@gmail.com>
2022-12-26 10:57:47 -0300

    practica de curso de NodeJS

---
+++ node/inicializarProyecto/app.js
@@ -0,0 +1,20 @@
+
+//console.log(Buffer(10));// salida que manera los elementos binarios
+// sirve para poder enviar recivir y modificar informacion binaria, transformar un archivo, imagen, pdf, etc
+//process.env.colorFavorito= "rojo"
+//console.log(process.env.colorFavorito);
+// TODO RESPECTO A LOS PROCESOS EL, como el sistema operativo (.platform)
+//.execPath para saber donde se encuentra el archiovo node
+//.env es el enviroment o entorno de trabajo
+
+//Importar paquetes externos como por ejemplo el de moment
+//var moment = require('moment');
+//console.log(moment('1999-12-31').format('DD/MM/YY').toString());
+
+//Llamar a modiulos locales, como por ejemplo miModulo.js
+/*var miModulo = require('./miModulo');
+miModulo.multiplicar(10);
+miModulo.dividir(60);*/
+
+
+
