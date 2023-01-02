fcb5c65e82a6dc18f76b1738d1b5b56c040233f3
TeremotoBettoni <mtbettoni@gmail.com>
2023-01-02 13:28:58 -0300

    practica de nodeJS

--- node/app.js
+++ node/app.js
@@ -1,4 +1,4 @@
-function mostrarSaludo(){
+/*function mostrarSaludo(){
     let valor1= "Hola";
     let valor2= " mundo";
 
@@ -6,4 +6,17 @@ function mostrarSaludo(){
 
     console.log(valor1+" "+valor2);
 }
-mostrarSaludo();
+mostrarSaludo();*/
+var http= require('http'); // se encarga de guiardar todos los metodos relacionados con la aplicacion y la internet
+
+var servidor = http.createServer( function(peticion, respuesta){
+    respuesta.writeHead(200, {'content-type': 'text/html'});
+    respuesta.write('Respuesta para la direccion: '+  peticion.url);
+
+
+    console.log("petición web");
+});
+
+servidor.listen(3000);
+
+console.log("Ejecutando servidor NoidJS");
--- node/inicializarProyecto/app.js
+++ node/inicializarProyecto/app.js
@@ -7,7 +7,7 @@
 //.execPath para saber donde se encuentra el archiovo node
 //.env es el enviroment o entorno de trabajo
 
-const { urlencoded } = require("express")
+//const { urlencoded } = require("express")
 
 //Importar paquetes externos como por ejemplo el de moment
 //var moment = require('moment');
@@ -30,3 +30,13 @@ console.log(util.isString(prueba));
 console.log(http);*/
 // uso de Nodemon para que los cambios en la aplicacion se reflecjen automaticos en la consola
 /*console.log("Resultado "+ Math.floor(Math.random()*200));*/
+
+var http= require('http'); // se encarga de guiardar todos los metodos relacionados con la aplicacion y la internet
+
+var servidor = http.createServer( function(){
+    console.log("petición web");
+});
+
+servidor.listen(3000);
+
+console.log("Ejecutando servidor NoidJS");
