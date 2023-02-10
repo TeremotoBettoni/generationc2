6fdf11330fb3d267e4863b0ad5202fcd0a7f5997
TeremotoBettoni <mtbettoni@gmail.com>
2023-02-10 11:39:15 -0300

    practica de lectura de archivos node

--- node/EXPRESS_BASICO2/LEERARCHIVOS/app.js
+++ node/EXPRESS_BASICO2/LEERARCHIVOS/app.js
@@ -1,5 +1,15 @@
 var fs = require("fs");
-
-var contenido = fs.readFileSync('./archivos/texto.txt', 'utf8'); /**utf8 es para ller los tipos de caracter */
-
-console.log(contenido)
+// para leer archivos de texto
+// var contenido = fs.readFileSync('./archivos/texto.txt', 'utf8'); /**utf8 es para ller los tipos de caracter */
+// para archivos de tipo avi o videos tarda en leerlos pero no en la misma sintaxis
+// creamos un proceso none bloquer
+fs.readFile('./archivos/texto.txt', 'utf8', (error, datos)=>{
+    if (error) {
+        console.log("error de lectura");
+        throw Error;
+    } else {
+        console.log(datos)
+    }
+    
+})
+//console.log(contenido);
