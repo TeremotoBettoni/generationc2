47e820d6f4316b0a002024479babd15ed0f93981
TeremotoBettoni <mtbettoni@gmail.com>
2023-02-13 14:58:24 -0300

    importar JSON

--- node/EXPRESS_BASICO2/LEER-JSON/app.js
+++ node/EXPRESS_BASICO2/LEER-JSON/app.js
@@ -1,3 +1,11 @@
-var libro = require('./archivos/libros.json');
+/*var libro = require('./archivos/libros.json');
 
 console.log(libro.autor) //podemos llamar solo ar archivo json o uan propiedas especifica dentro de este
+*/
+// para impostar el archivo de manera asincrona
+var fs = require('fs');
+
+fs.readFile('./archivos/libros.json', (error, datos)=>{
+ // antes debemos datos forma o parcear 
+    console.log(JSON.parse(datos).autor);
+})
