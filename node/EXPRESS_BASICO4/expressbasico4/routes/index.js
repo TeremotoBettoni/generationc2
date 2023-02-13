6a58d3bdf84de9eb1881daf3a50bf954fa0f3a2e
TeremotoBettoni <mtbettoni@gmail.com>
2023-02-13 18:10:53 -0300

    Desplegar JSON en Pantalla

---
+++ node/EXPRESS_BASICO4/expressbasico4/routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  fs.readFile('./public/json/libros.json', (error, datos)=>{
+    if (error) {
+      res.render("error de lectura");
+    } else {
+      res.render('index', { datos: JSON.parse(datos)});
+    }
+  })
+
+});
+
+module.exports = router;
